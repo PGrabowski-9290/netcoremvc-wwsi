aba67334ba0b784dd426c1fae6ea9b818060c464
PGrabowski-9290 <92215276+PGrabowski-9290@users.noreply.github.com>
2021-11-14 12:23:07 +0100

    Dodanie BookService i menu

---
+++ 3_powtorka/Library/Library.ConsoleApp/BooksService.cs
@@ -0,0 +1,54 @@
+﻿using Library.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.ConsoleApp
+{
+    internal class BooksService
+    {
+        public void AddBook()
+        {
+            Book book = new Book();
+            Console.Write("\n\n\n \tPodaj danne książki:");
+            Console.WriteLine("Tytul");
+            book.Title = Console.ReadLine();
+            Console.WriteLine("Autor");
+            book.Author = Console.ReadLine();
+            Console.WriteLine("Rok publikacji");
+            book.PublicationYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ISBN");
+            book.ISBN = Console.ReadLine();
+            Console.WriteLine("Ilosc magazynowa");
+            book.ProductAvailable = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cena");
+            book.Price = Convert.ToDecimal(Console.ReadLine());
+        }
+
+        public void RemoveBook()
+        {
+            string bookToRemove = "";
+            Console.WriteLine("Podaj tytul ksiazki do usuniecia:");
+            bookToRemove = Console.ReadLine();
+        }
+
+        public void ListBooks()
+        {
+            Console.WriteLine("Tutaj pojawi sie lista ksiazek");
+
+        }
+
+        public void ChangeState()
+        {
+            string title;
+            int state;
+            Console.WriteLine("Podaj tytul ksiazki ktorej chcesz zmienic stan (ile dodac/odjac od obecnego stanu)");
+            title = Console.ReadLine();
+            Console.WriteLine("Podaj o ile zmienic stan magazynowy");
+            state = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ 3_powtorka/Library/Library.ConsoleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Library.Domain;
+using Library.Persistence;
+
+namespace Library.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Czysty kod", "Robert C. Martin", 2014, "978-83-283-0234-1", 3, (decimal)52.55);
+            BooksRepository repository = new BooksRepository();
+
+            Console.WriteLine("Login: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Hasło: ");
+            string password = Console.ReadLine();
+
+            if(login == "Admin" && password == "password")
+            {
+                Console.Clear();
+                Console.WriteLine("Access Granted");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Access Denied");
+                Environment.Exit(0);
+            }
+
+            BooksService bookService = new BooksService();
+            string command = "";
+            do
+            {
+                
+                Console.Write("\n\tLISTA KOMEND:\n\r- dodaj\n\r- usun\n\r- wypisz\n\r- zmien\n\r- dodaj zamowienie\n\r- lista zamowien\n- wyjdz\n\n\r#>");
+                command = Console.ReadLine();
+                switch(command)
+                {
+                    case "dodaj":
+                        bookService.AddBook();
+                        Console.WriteLine("Press AnyKey...");
+                        Console.ReadKey();
+                        break;
+                    case "usun":
+                        bookService.RemoveBook();
+                        Console.WriteLine("Press AnyKey...");
+                        Console.ReadKey();
+                        break;
+                    case "wypisz":
+                        bookService.ListBooks();
+                        Console.WriteLine("Press AnyKey...");
+                        Console.ReadKey();
+                        break;
+                    case "zmien":
+                        bookService.ChangeState();
+                        Console.WriteLine("Press AnyKey...");
+                        Console.ReadKey();
+                        break;
+                    case "dodaj zamowienie":
+                        Console.WriteLine("Proba dodania nowego zamowienia");
+                        Console.WriteLine("Press AnyKey...");
+                        Console.ReadKey();
+                        break;
+                    case "lista zamowien":
+                        Console.WriteLine("Proba wypisania wszystkich zamowien");
+                        Console.WriteLine("Press AnyKey...");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("\r\n\r\r\nKomenda nie wspierana");
+                        break;
+                }
+                Console.Clear();
+            } while (command != "wyjdz");
+        }
+    }
+}
