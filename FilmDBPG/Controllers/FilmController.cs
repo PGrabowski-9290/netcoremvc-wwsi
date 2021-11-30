b77404027704562afe112e9db181083c24b7bae1
PGrabowski-9290 <92215276+PGrabowski-9290@users.noreply.github.com>
2021-12-01 00:14:19 +0100

    Add my FilmDB project

---
+++ FilmDBPG/Controllers/FilmController.cs
@@ -0,0 +1,76 @@
+﻿using FilmDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        public IActionResult Index()
+        {
+            FilmManager filmManager = new FilmManager();
+            List<FilmModel> films = filmManager.GetFilms();
+
+            return View(films);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FilmModel film)
+        {
+            FilmManager filmManager = new FilmManager();
+            filmManager.AddFilm(film);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+            return View(film);
+        }
+
+        public IActionResult RemoveConfirm(int id)
+        {
+            var manager = new FilmManager();
+            try
+            {
+                manager.RemoveFilm(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var manager = new FilmManager();
+            var film = manager.GetFilm(id);
+
+            return View(film);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(FilmModel film)
+        {
+            var manager = new FilmManager();
+            manager.UpdateFilm(film);
+
+            return View(film);
+        }
+    }
+}
---
+++ FilmDBPG/FilmManager.cs
@@ -0,0 +1,106 @@
+﻿using FilmDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB
+{
+    public class FilmManager
+    {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            using(FilmContext context = new FilmContext())
+            {
+                context.Films.Add(filmModel);
+                try
+                {                    
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    filmModel.ID = 0;
+                    context.Films.Add(filmModel);
+                    context.SaveChanges();
+                }
+            }
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            using( FilmContext context = new FilmContext())
+            {
+                try
+                {
+                    var delFilm = context.Films.SingleOrDefault(f => f.ID == id);
+                    _ = context.Films.Remove(delFilm);
+                    context.SaveChanges();
+                }
+                catch(Exception)//DbUpdateException
+                {
+                    //Error message TODO
+                }
+            }
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            using(FilmContext context = new FilmContext())
+            {
+                try
+                {
+                    context.Update(filmModel);
+                    context.SaveChanges();
+                }
+                catch(Exception)
+                {
+                    //TODO
+                }
+            }
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            using (FilmContext context = new FilmContext())
+            {
+                try
+                {
+                    var updatedFilm = context.Films.Where(f => f.ID == id).SingleOrDefault();
+                    if (newTitle != null)
+                    {
+                        updatedFilm.Title = newTitle;
+                    }
+                    else
+                    {
+                        updatedFilm.Title = "Brak Tytułu";
+                    }
+
+                    this.UpdateFilm(updatedFilm);
+                }
+                catch (Exception)
+                {
+                    //TODO
+                }
+            }
+            return this;
+        }
+
+        public FilmModel GetFilm(int id)
+        {
+            using FilmContext context = new FilmContext();
+            FilmModel film = context.Films.SingleOrDefault(f => f.ID == id);
+            return film;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            using FilmContext context = new FilmContext();
+            return context.Films.ToList();
+        }
+    }
+
+}
